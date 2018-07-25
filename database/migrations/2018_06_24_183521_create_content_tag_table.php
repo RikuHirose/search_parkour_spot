0856c61c74f89580c4e3e8eb45fb6a408f4f884d
RikuHirose <rikuparkour9@gmail.com>
2018-07-25 18:48:37 +0700

    changename

--- database/migrations/2018_06_24_183521_create_content_tag_table.php
+++ database/migrations/2018_06_24_183521_create_content_tag_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateContentTagTable extends Migration
+class CreateContentTagTable2 extends Migration
 {
     /**
      * Run the migrations.
