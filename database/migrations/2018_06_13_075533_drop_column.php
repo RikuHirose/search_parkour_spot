7037811b24c1311eb57cdb849afa541b1afeb055
RikuHirose <rikuparkour9@gmail.com>
2018-07-25 18:46:32 +0700

    changename

--- database/migrations/2018_06_13_075533_drop_column.php
+++ database/migrations/2018_06_13_075533_drop_column.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class DropColumn extends Migration
+class DropColumn3 extends Migration
 {
     /**
      * Run the migrations.
