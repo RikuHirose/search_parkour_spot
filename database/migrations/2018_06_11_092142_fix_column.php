1a9d102d9524f7d4a4a79d6aaadd9e1455b1ecc9
RikuHirose <rikuparkour9@gmail.com>
2018-07-25 18:40:48 +0700

    change.name

--- database/migrations/2018_06_11_092142_fix_column.php
+++ database/migrations/2018_06_11_092142_fix_column.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class FixColumn extends Migration
+class FixColumn2 extends Migration
 {
     /**
      * Run the migrations.
